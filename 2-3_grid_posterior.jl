5dd60442e6f599c76c71a8b6372aac05dd457bbb
Brian Kent <2701842+bpkent@users.noreply.github.com>
2021-05-11 18:19:00 -0500

    Add code blocks 2.4 and 2.5 to the block 2.3 script.

--- 2-3_grid_posterior.jl
+++ 2-3_grid_posterior.jl
@@ -2,14 +2,25 @@
 using Distributions
 using Plots
 
-proba_grid = range(0, 1, length=20)
+grid_size = 40
+proba_grid = range(0, 1, length=grid_size)
 
-prior = ones((20))
+## Code block 2.5
+# prior = ones((grid_size))
+# prior = collect(proba_grid) .>= 0.5
+prior = exp.(-5 * abs.(collect(proba_grid) .- 0.5))
+
+plot(
+    proba_grid, prior, title="40 Points", xlabel="Probability of water", 
+    ylabel="Prior probability", lw=3
+)
+
 likelihood = [pdf(Binomial(9, p), 6) for p in proba_grid]
 posterior = prior .* likelihood
 posterior_proba = posterior / sum(posterior)
 
+## Code block 2.4
 plot(
-    proba_grid, posterior_proba, title="20 Points", xlabel="Probability of water", 
+    proba_grid, posterior_proba, title="40 Points", xlabel="Probability of water", 
     ylabel="Posterior probability", lw=3
 )
